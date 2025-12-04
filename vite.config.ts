c613c5a739a19ce03a5a2b92c4d6f7a1fb62ca79
joaovalmeida <joao.v.p.almeida@gmail.com>
2025-12-04 20:49:53 +0000

    build a different html file

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,26 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import fs from 'fs/promises';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base:"finanssas",
+      base:"/finanssas",
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
-      plugins: [react()],
+      plugins: [
+        react(),
+        {
+          name: 'index-html-build-replacement',
+          apply: 'build',
+          async transformIndexHtml() {
+            return await fs.readFile('./index_routed.html', 'utf8');
+          },
+        },
+      ],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
